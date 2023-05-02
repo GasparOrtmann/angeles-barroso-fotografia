8045260a4a15696cabcf8d5f0f7ac24696198800
Gaspar Ortmann <gasparortmann@gmail.com>
2023-05-02 15:52:38 -0300

    shadow nav bar mobile

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const nav = document.querySelector("#nav");
 const open = document.querySelector("#open");
 const close = document.querySelector("#close");
+const img = document.querySelector("#header");
+const navlist = document.querySelector("#nav-list");
 
 open.addEventListener("click", () => {
     nav.classList.add("visible");
+    img.classList.add("shadow");
 })
 
 close.addEventListener("click", () => {
     nav.classList.remove("visible");
+    img.classList.remove("shadow");
+})
+
+navlist.addEventListener("click", () => {
+    nav.classList.remove("visible");
+    img.classList.remove("shadow");
 })
